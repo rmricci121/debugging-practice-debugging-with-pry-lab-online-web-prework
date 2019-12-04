d9af1d17965697d57754f3857d0175f2dcc05627
Robert M Ricci <rmricci121@gmail.com>
2019-12-04 01:11:42 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -13,9 +13,9 @@ end
 def selection(num)
   if  num = 1
      "YUM YUM MUNCH MUNCH MUNCH"
-  else num = 2  
+  elsif num = 2  
      "HAM HAM HAM IN MY TUMMY"
-  end
+  
 end
 
 def runner
@@ -23,5 +23,5 @@ def runner
   selection(get_user_input)
 end
 
-binding.pry 
+#binding.pry 
 
